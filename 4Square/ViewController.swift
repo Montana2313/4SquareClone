acb8d2d1356594f25eca579c774308e0b468417e
Özgür Elmaslı <ozgurelmasli@Ozgur-iMac.local>
2018-01-18 13:05:35 +0300

    Initial Commit

---
+++ 4Square/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4Square
+//
+//  Created by Özgür  Elmaslı on 18.01.2018.
+//  Copyright © 2018 Özgür  Elmaslı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
